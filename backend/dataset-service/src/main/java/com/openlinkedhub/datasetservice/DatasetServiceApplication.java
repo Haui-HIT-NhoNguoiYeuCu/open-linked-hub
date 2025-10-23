425377efc54f766536506e518891c3ccb6e37fd3
hoangnh0402 <hoangnh4204@gmail.com>
2025-10-23 18:54:16 +0700

    hoangnh0402/dataset-service/feat: add base

---
+++ backend/dataset-service/src/main/java/com/openlinkedhub/datasetservice/DatasetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.openlinkedhub.datasetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasetServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatasetServiceApplication.class, args);
+    }
+
+}
